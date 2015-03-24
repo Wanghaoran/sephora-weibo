66ca491324cc1e2f514b60c6eb31ec18060dfa40
WangHaoRan <whrlmc@139.com>
2015-03-24 20:23:49 +0800

    123123

--- application/views/statistics.php
+++ application/views/statistics.php
@@ -21,6 +21,8 @@
     <li><h2>礼包生成数量：<span style="color:blue;"><?=$question_num;?></span></h2></li>
     <li><h2>参与互动人数：<span style="color:blue;"><?=$p_num;?></span></h2></li>
     <li><h2>回答问题数量：<span style="color:blue;"><?=$ex_num;?></span></h2></li>
+    <li><h2>PV/UV数据：<a href="http://new.cnzz.com/v1/login.php?siteid=1254626964" target="_blank" style="color:blue;">点击查看</a>&nbsp;&nbsp;&nbsp;&nbsp;查看密码：<span style="color: blue;">sephora_zoe</span></h2></li>
+    <li><h2>数据更新日期：：<span style="color: blue;"><?=date('Y-m-d H:i:s');?></span></h2></li>
 </ul>
 
 </body>
