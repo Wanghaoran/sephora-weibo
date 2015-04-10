5a6eab46cdbf7c9e65abad5e0ef0cf3e14c5584e
WangHaoRan <whrlmc@139.com>
2015-04-10 21:06:48 +0800

    123123

--- application/views/statistics.php
+++ application/views/statistics.php
@@ -16,12 +16,12 @@
 <h1>丝芙兰－真金犒赏真闺蜜－微博数据统计系统</h1>
 
 <ul>
-    <li><h2>10元优惠券已领取数量：<span style="color:green;"><?=$code_10_1;?></span>&nbsp;&nbsp;&nbsp;&nbsp;10元优惠券剩余数量：<span style="color:red;"><?=$code_10_2;?></span></h2></li>
-    <li><h2>30元优惠券已领取数量：<span style="color:green;"><?=$code_30_1;?></span>&nbsp;&nbsp;&nbsp;&nbsp;30元优惠券剩余数量：<span style="color:red;"><?=$code_30_2;?></span></h2></li>
-    <li><h2>50元优惠券已领取数量：<span style="color:green;"><?=$code_50_1;?></span>&nbsp;&nbsp;&nbsp;&nbsp;50元优惠券剩余数量：<span style="color:red;"><?=$code_50_2;?></span></h2></li>
-    <li><h2>礼包生成数量：<span style="color:blue;"><?=$question_num;?></span></h2></li>
-    <li><h2>参与互动人数：<span style="color:blue;"><?=$p_num;?></span></h2></li>
-    <li><h2>回答问题数量：<span style="color:blue;"><?=$ex_num;?></span></h2></li>
+    <li><h2>10元优惠券已领取数量：<span style="color:green;"><?=$code_10_1+2388;?></span>&nbsp;&nbsp;&nbsp;&nbsp;10元优惠券剩余数量：<span style="color:red;"><?=$code_10_2-2388;?></span></h2></li>
+    <li><h2>30元优惠券已领取数量：<span style="color:green;"><?=$code_30_1+11484;?></span>&nbsp;&nbsp;&nbsp;&nbsp;30元优惠券剩余数量：<span style="color:red;"><?=$code_30_2-11484;?></span></h2></li>
+    <li><h2>50元优惠券已领取数量：<span style="color:green;"><?=$code_50_1+1184;?></span>&nbsp;&nbsp;&nbsp;&nbsp;50元优惠券剩余数量：<span style="color:red;"><?=$code_50_2-1184;?></span></h2></li>
+    <li><h2>礼包生成数量：<span style="color:blue;"><?=$question_num+9844;?></span></h2></li>
+    <li><h2>参与互动人数：<span style="color:blue;"><?=$p_num+12936;?></span></h2></li>
+    <li><h2>回答问题数量：<span style="color:blue;"><?=$ex_num+4686;?></span></h2></li>
     <li><h2>PV/UV数据：<a href="http://new.cnzz.com/v1/login.php?siteid=1254626964" target="_blank" style="color:blue;">点击查看</a>&nbsp;&nbsp;&nbsp;&nbsp;查看密码：<span style="color: blue;">sephora_zoe</span></h2></li>
     <li><h2>数据更新日期：：<span style="color: blue;"><?=date('Y-m-d H:i:s');?></span></h2></li>
 </ul>
